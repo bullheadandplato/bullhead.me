f1f37c97bf751d74e808c12b53072976e10cca0e
Osama aka bullhead <m.osamabinomar@gmail.com>
2017-09-22 03:23:17 +0500

    side bar like profile done

--- src/LeftCard.js
+++ src/LeftCard.js
@@ -6,9 +6,26 @@ class LeftCard extends Component {
         return (
             <div className={'leftCard'}>
                 <div className={'leftDetails'}>
-                    <Avatar skypeId={'m.osamabinomar@gmail.com'} size={100} round={true}/>
-                    <h4>Osama Bin Omar</h4>
-                    <i className={"fa fa-github"} aria-hidden={"true"}/>
+                    <Avatar
+                        src={"https://avatars0.githubusercontent.com/u/15232745?v=4&s=400&u=8114e15bc03228a9c0f04ce4c092cb703813ba60"}
+                        size={70} round={true}/>
+                    <h3 id={"name"}>Osama Bin Omar (bullhead)</h3>
+                    <h6 id={"now"}>Swift & React & Kotlin</h6>
+                    <div className={"icons"}>
+                        <a href={'https://github.com/mosamabinomar'} target={"_blank"}>
+                            <i className={"fa fa-github fa-lg"} aria-hidden={"true"}/>
+                        </a>
+                        <a href={'https://twitter.com/bullhead_void'} target={"_blank"}>
+                            <i className={"fa fa-twitter fa-lg"} aria-hidden={"true"}/>
+                        </a>
+                        <a href={'https://www.linkedin.com/in/osama-bin-omar-a2124b144/'} target={"_blank"}>
+                            <i className={"fa fa-linkedin-square fa-lg"} aria-hidden={"true"}/>
+                        </a>
+                        <a href={'mailto:m.osamabinomar@gmail.com'}>
+                            <i className={"fa fa-envelope fa-lg"} aria-hidden={"true"}/>
+                        </a>
+
+                    </div>
                 </div>
             </div>
         );
