c02de6f4d550cc52513419ef6448cd1afb8091bc
Osama aka bullhead <m.osamabinomar@gmail.com>
2017-09-22 03:42:49 +0500

    java

--- src/LeftCard.js
+++ src/LeftCard.js
@@ -10,7 +10,7 @@ class LeftCard extends Component {
                         src={"https://avatars0.githubusercontent.com/u/15232745?v=4&s=400&u=8114e15bc03228a9c0f04ce4c092cb703813ba60"}
                         size={70} round={true}/>
                     <h3 id={"name"}>Osama Bin Omar (bullhead)</h3>
-                    <h6 id={"now"}>Swift & React & Kotlin</h6>
+                    <h6 id={"now"}>Java & Swift & React & Kotlin</h6>
                     <div className={"icons"}>
                         <a href={'https://github.com/mosamabinomar'} target={"_blank"}>
                             <i className={"fa fa-github fa-lg"} aria-hidden={"true"}/>
